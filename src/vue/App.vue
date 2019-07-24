7bc2ec55434ea72e32a76c0dd8c7972f89bcdaa2
Tomas Roberto Scheel <tomasrscheel@gmail.com>
2019-07-24 10:40:32 -0500

    Updated config to account for sass/scss loading

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,16 +1,20 @@
 <template>
-    <div>
-        <div>Hello {{name}}!</div>
-        Name: <input v-model="name" type="text">
+    <div class="md-layout">
+        <Sidebar />
+        <div class="md-layout-item">
+            <div>Hello {{name}}!</div>
+            Name: <input v-model="name" type="text">
+        </div>
     </div>
 </template>
 
 <script lang="ts">
     import Vue from "Vue";
     import Component from "vue-class-component";
+    import Sidebar from "./base/Sidebar.vue";
 
     @Component({
-        components: { }
+        components: { Sidebar }
     })
     export default class App extends Vue {
         name: string = "World";
