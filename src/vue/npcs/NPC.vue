785b3e45d6fc8e8231debc730d38095711d3f499
Tomas Roberto Scheel <tomasrscheel@gmail.com>
2019-07-29 22:34:20 -0500

    Finalized direction to take for editing NPC dialogs

--- src/vue/npcs/NPC.vue
+++ src/vue/npcs/NPC.vue
@@ -17,87 +17,14 @@
                     <input ref="fileinput" type="file" accept="*.json" style="display: none" @change="loadFile" />
                 </md-button>
 
-                <md-button class="md-icon-button">
+                <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
                     <md-icon>cloud_download</md-icon>
                     <md-tooltip md-direction="right">Download</md-tooltip>
                 </md-button>
             </md-speed-dial-content>
         </md-speed-dial>
-        <div class="content">
-            <md-card v-bind:key="idx" v-for="(dialog, idx) in dialogs" class="md-primary" md-with-hover>
-                <md-card-header>
-                    <md-card-header-text>
-                        <div class="md-title">{{ dialog.id }}</div>
-                    </md-card-header-text>
-                    <md-menu md-size="big" md-direction="bottom-end">
-                        <md-button class="md-icon-button" md-menu-trigger>
-                            <md-icon>add_circle_outline</md-icon>
-                        </md-button>
-
-                        <md-menu-content>
-                            <md-menu-item>
-                                <span>Dynamic Line</span>
-                                <md-icon>record_voice_over</md-icon>
-                            </md-menu-item>
-
-                            <md-menu-item @click="addSpeakerEffect(dialog)">
-                                <span>Speaker Effect</span>
-                                <md-icon>surround_sound</md-icon>
-                            </md-menu-item>
-
-                            <md-menu-item>
-                                <span>Response</span>
-                                <md-icon>message</md-icon>
-                            </md-menu-item>
-                        </md-menu-content>
-                    </md-menu>
-                </md-card-header>
-                <md-card-expand>
-                    <md-card-expand-content>
-                        <md-card-content>
-                            <md-card v-if="!!dialog.speaker_effect">
-                                <md-card-header>
-                                    <md-card-header-text>
-                                        <div class="md-subtitle">
-                                            <md-icon>surround_sound</md-icon>
-                                            {{ dialog.speaker_effect.sentinel }}
-                                        </div>
-                                    </md-card-header-text>
-                                    <md-menu md-size="big" md-direction="bottom-end">
-                                        <md-button class="md-icon-button" md-menu-trigger>
-                                            <md-icon>add_circle_outline</md-icon>
-                                        </md-button>
-
-                                        <md-menu-content>
-                                            <md-menu-item @click="addCondition(dialog.speaker_effect)">
-                                                <span>Condition</span>
-                                                <md-icon>flag</md-icon>
-                                            </md-menu-item>
-
-                                            <md-menu-item>
-                                                <span>Effect</span>
-                                                <md-icon>notifications</md-icon>
-                                            </md-menu-item>
-                                        </md-menu-content>
-                                    </md-menu>
-                                </md-card-header>
-                                <md-card-content>
-                                    <md-card v-if="!!dialog.speaker_effect.condition">
-                                        <Condition v-bind:key="conid" v-for="(con, conid) in dialog.speaker_effect.condition" :conditional="con" />                                                
-                                    </md-card>
-                                </md-card-content>
-                            </md-card>
-                        </md-card-content>
-                    </md-card-expand-content>
-                    <md-card-actions md-alignment="space-between">
-                        <md-card-expand-trigger>
-                            <md-button class="md-icon-button">
-                                <md-icon>keyboard_arrow_down</md-icon>
-                            </md-button>
-                        </md-card-expand-trigger>
-                    </md-card-actions>
-                </md-card-expand>
-            </md-card>
+        <div class="content" ref="paperContainer">
+            
         </div>
     </div>
 </template>
@@ -110,12 +37,101 @@
     import ConditionalLine from "../../models/npcs/ConditionalLine";
     import { SimpleConditional, DeclarativeConditional } from "../../models/npcs/Enums";
     import Condition from "./Condition.vue";
+    import jointTS from "jointjs";
+    let joint = require("jointjs");
 
     @Component({
         components: { Condition }
     })
     export default class NPC extends Vue {
         public dialogs:Talk[] = [ new Talk() ];
+        public menuVisible:boolean = false;
+
+        private baseRect?:joint.shapes.standard.Rectangle;
+        private baseLink?: joint.shapes.standard.Link;
+
+        private graph?:joint.dia.Graph;
+        private paper?:joint.dia.Paper;
+
+        private element?:Element;
+
+        mounted() {
+            // this will be pulled out into a separate object once I'm done here
+            this.graph = new joint.dia.Graph;
+
+            this.element = this.$refs.paperContainer as Element;
+
+            this.paper = new joint.dia.Paper({
+                el: this.$refs.paperContainer,
+                model: this.graph ,
+                width: this.element.clientWidth,
+                height: this.element.clientHeight,
+                gridSize: 1
+            } as any);
+
+            let prevPosition:{x: number, y:number} | undefined = undefined;
+            let curPosition:{x: number, y:number} | undefined = undefined;
+            let translate:{x: number, y:number} | undefined = undefined;
+            
+            let innerPaper = this.paper!;
+
+            (this.paper! as any).on('blank:pointermove',
+                function(evt:any, x:number, y:number) {
+                    let curPosition = {x: evt.originalEvent.clientX, y: evt.originalEvent.clientY};
+
+                    if (!prevPosition || !translate){
+                        prevPosition = {
+                            x: curPosition.x,
+                            y: curPosition.y
+                        };
+                        translate = {
+                            x: innerPaper.translate().tx,
+                            y: innerPaper.translate().ty
+                        };
+                    } else {
+                        innerPaper.translate(
+                            translate.x + curPosition.x - prevPosition.x,
+                            translate.y + curPosition.y - prevPosition.y
+                        );
+                    }
+                }
+            );
+
+            (this.paper! as any).on('blank:pointerup',
+                function(evt:any, x:number, y:number) {
+                    prevPosition = undefined;
+                }
+            );
+
+            this.baseRect = new joint.shapes.standard.Rectangle();
+            this.baseRect!.position(this.element.clientWidth / 2 - 200, 0);
+            this.baseRect!.resize(400, 40);
+            this.baseRect!.attr({
+                body: {
+                    fill: '#2C3E50',
+                    rx: 5,
+                    ry: 5,
+                    strokeWidth: 2
+                },
+                label: {
+                    fill: '#3498DB',
+                    fontSize: 18,
+                    fontWeight: 'bold',
+                    fontVariant: 'small-caps'
+                }
+            });
+
+            this.baseLink = new joint.shapes.standard.Link();
+            this.baseLink!.router('orthogonal');
+            this.baseLink!.connector('rounded');
+            this.baseLink!.attr({
+                line: {
+                    stroke: 'gray'
+                }
+            });
+
+            this.createTalkDiagram();
+        }
 
         loadFile() {
             let file = ((this.$refs.fileinput as any).files as FileList)[0];
@@ -132,8 +148,36 @@
 
         }
 
+        createTalkDiagram() {
+            this.graph!.clear();
+
+            for(let i:number = 0; i < this.dialogs.length; i++) {  
+                if (this.dialogs[i].type !== "talk_topic") continue; // break out if this isnt a talk topic
+                
+                let rect = this.baseRect!.clone() as joint.shapes.standard.Rectangle;
+                rect.translate(0, i * 100);
+
+                let isArray = Array.isArray(this.dialogs[i].id);
+
+                let title:string = isArray ? 
+                        (this.dialogs[i].id as string[])[0] :
+                        this.dialogs[i].id as string;
+
+                title = [
+                    isArray ? "[" : "",
+                    title.length > 36 ? `${title.substring(0, 30)}...` : title,
+                    isArray ? `, +${(this.dialogs[i].id as string[]).length}]` : ""
+                ].join('');
+
+                rect.attr("label/text", title);
+                rect.addTo(this.graph!);
+            }
+        }
+
         parseDialog(dialog:string) {
-            this.dialogs = (JSON.parse(dialog) as Talk[]);
+            let temp = (JSON.parse(dialog) as Talk[]);
+            this.dialogs = temp.filter(x => x.type === "talk_topic" );
+            this.createTalkDiagram();
         }
 
         uploadFile() {
@@ -163,10 +207,17 @@
 
     .container {
         position: relative;
+        height: 100%;
+        width: 100%
     }
 
     .content {
         z-index: 1;
-        margin-left: 5vw;
+        height: 90%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background: url("/imgs/grid.png");
     }  
 </style>
